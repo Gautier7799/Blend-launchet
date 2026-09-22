86301672f172af87ea5744b31ea48e94abc8a3b3
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-09-22 21:14:10 +0100

    Add Firebase dependencies to build.gradle.kts
    Added Firebase libraries for authentication, Firestore, and storage.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // مكتبات Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
 }
