d6a7b384bff5c699903fefe42e9ede03d59f2601
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-09-22 20:10:37 +0100

    Update Gradle plugin versions in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+// build.gradle.kts (المجلد الرئيسي للمشروع)
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    // إصدار KSP متوافق مع Kotlin
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
